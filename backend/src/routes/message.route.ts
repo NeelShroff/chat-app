c2e673fa791bef7c1c99b5f0deaf1289ad87d63e
Neel Shroff <neelshroff03@gmail.com>
2024-08-23 01:22:10 +0530

    message routes and controllers added

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import protectRoute from "../middleware/procectRoute.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controller/message.controller.js";
 
 
 const router = express.Router();
 
-router.post("/conversation",(req,res)=>{
-    res.send("conversation route");
-})
-;
+router.get("/conversations",protectRoute,getUsersForSidebar)
+router.get("/:id",protectRoute,getMessages)
+router.post("/send/:id",protectRoute,sendMessage)
 export default router
 
