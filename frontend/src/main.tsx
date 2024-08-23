eabd20620435084f712bc56c675e3f18d9f48907
Neel Shroff <neelshroff03@gmail.com>
2024-08-24 00:46:10 +0530

    ..

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+ import { AuthContextProvider } from "./context/AuthContext.tsx";
+// import SocketContextProvider from "./context/SocketContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	// <React.StrictMode>
+	<BrowserRouter>
+		{/* <AuthContextProvider>
+			<SocketContextProvider>
+				<App />
+			</SocketContextProvider>
+		</AuthContextProvider> */}
+		 <AuthContextProvider>
+		 <App/>
+		 </AuthContextProvider>
+		
+	</BrowserRouter>
+	// </React.StrictMode>
+);
